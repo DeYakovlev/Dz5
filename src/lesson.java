97109b4fa2c95c4e2a979ece6ad45c4c5e8a46d7
DeYakovlev <99YaDe@gmail.com>
2022-05-07 20:15:32 +0300

    Initial commit

---
+++ src/lesson.java
@@ -0,0 +1,14 @@
+public class lesson {
+    public static void main(String[] args) {
+        int counterX = 0;
+        int x = 4;
+        for (long i = 40; i <= 60; i++) {
+            if (i % x == 0) {
+                counterX++;{
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+}
+
